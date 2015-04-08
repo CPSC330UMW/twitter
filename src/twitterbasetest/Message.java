d870919e29371337fe5e6da51446c365e52f9c81
Tommy <Tommy@Tommy-Laptop>
2015-04-08 15:35:13 -0400

    A solid start

---
+++ src/twitterbasetest/Message.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package twitterbasetest;
+
+/**
+ *
+ * @author Tommy
+ */
+public class Message 
+{
+    public Message(String words)
+    {
+        String message = words;
+    }
+}
